4b368d7b61b860390712164c71b7720ce7619b7b
Keaton Burleson <keaton.burleson@me.com>
2017-09-08 22:15:34 -0500

    Added Geofire and Bridging-Header

---
+++ ios/yum/yum/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  yum
+//
+//  Created by Keaton Burleson on 9/8/17.
+//  Copyright © 2017 poeen. All rights reserved.
+//
+
+#import "GeoFire.h"
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */

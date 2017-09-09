8427b12ddcab55dd5bf31648ed1d4bceee05c000
Kareem Dasilva <kdasilv2@its.jnj.com>
2017-09-09 17:42:45 -0500

    added login screen

---
+++ ios/yum/yum/LoginViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LoginViewController.swift
+//  yum
+//
+//  Created by Kareem Dasilva on 9/9/17.
+//  Copyright © 2017 poeen. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+class LoginViewController: UIViewController {
+    var ref: DatabaseReference!
+    //Textfields
+    @IBOutlet var emailTextField: TextFields!
+    @IBOutlet var passwordTextfield: TextFields!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logic(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextfield.text {
+            Auth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+                
+                if error != nil {
+                    print("there is an error with logging in")
+                    let alertController = UIAlertController(title: "Sorry", message: "Either your password or email is incorrect, try again.", preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+                    {
+                        (result : UIAlertAction) -> Void in
+                        print("You pressed OK")
+                    }
+                    alertController.addAction(okAction)
+                    self.present(alertController, animated: true, completion: nil)
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+                        if error == nil {
+                            print("Can't sign in user")
+                        } else {
+                            self.performSegue(withIdentifier: "LoginHome", sender: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
